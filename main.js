f5a79aab39ec8ed94cb70e71d458b9250d60d854
NicholasKyleHoffman <hoffman.nick89@gmail.com>
2020-05-03 12:11:31 -0700

    Updates to package.json, main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Connected to: mysql-nodejs-integration project\n');
+}).listen(6000);
+
+console.log('Server started');
+
+
 var mysql = require('mysql');
 var conn = mysql.createConnection({
   host      : 'localhost',  // mysql db name
   user      : 'root',       // mysql db username
   password  : '',           // mysql db password
-  database  : 'dummy_db'    // myql db name
+  database  : 'test_db'    // myql db name
 })
 
 conn.connect();
