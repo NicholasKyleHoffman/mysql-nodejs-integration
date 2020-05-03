71d9412fca0755ea32e43fa10bdbdc0aa37e2b63
NicholasKyleHoffman <hoffman.nick89@gmail.com>
2020-05-03 12:26:01 -0700

    updated mysql version

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-var http = require('http');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost', //mysql database name
+  user     : 'root', //mysql database username
+  password : '', //mysql database password
+  database : 'dummy_db' //mysql database name
+});
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Connected to: mysql-nodejs-integration project\n');
-}).listen(6000);
+// connection.connect();
 
-console.log('Server started');
-
-
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-  host      : 'localhost',  // mysql db name
-  user      : 'root',       // mysql db username
-  password  : '',           // mysql db password
-  database  : 'test_db'    // myql db name
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
 })
 
-conn.connect();
+// var http = require('http');
+//
+// http.createServer(function (request, response) {
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     response.end('Connected to: mysql-nodejs-integration project\n');
+// }).listen(6000);
+//
+// console.log('Server started');
 
-conn.connect(function(err) {
-  if (err) throw error
-  console.log("You are now connected!!!!....")
-});
+// CREATE TABLE IF NOT EXISTS `employee` (
+// `id` int(11) NOT NULL COMMENT 'primary key',
+//   `employee_name` varchar(255) NOT NULL COMMENT 'employee name',
+//   `employee_salary` double NOT NULL COMMENT 'employee salary',
+//   `employee_age` int(11) NOT NULL COMMENT 'employee age'
+// ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COMMENT='datatable demo table';
